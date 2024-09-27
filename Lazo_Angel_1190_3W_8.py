30f127a7be67f0143df4c174832f1c99ed34614f
Lazoangel <a.23308051281190@cbtis128.edu.mx>
2024-09-27 08:15:25 -0600

    Add files via upload

---
+++ Lazo_Angel_1190_3W_8.py
@@ -0,0 +1,6 @@
+print ("Lazo Jimenez Angel Jesus 3W")
+Num_Natu = int (input("ingresar un numero natural: ")) #Le pide al usuario que ingrese un numero natural
+if Num_Natu %2 == 0: #Esta linea lo que hace es que verifica si el numero es par o impar
+     print(f"El numero {Num_Natu} es par") #Este mensaje aparece si el numero es par
+else:
+     print(f"El numero {Num_Natu} es impar") #Este mensaje aparece si el numero es impar 
